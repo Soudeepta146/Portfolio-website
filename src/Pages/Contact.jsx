bef42e1f554f361f35b45c189cc384c55654d73e
Soudeepta Bhattacharjee <soudeepta2004bhattacharjee@gmail.com>
2026-04-13 00:59:08 +0530

    Improved About, Contact and UI design

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,9 @@
 function Contact(){
     return(
         <section id="contact" className="section">
-            <h2>Contact</h2>
+            <h2>Contact Me</h2>
             <p>Email:soudeepta2004bhattacharjee@gmail.com</p>
+            <p>GitHub: https://github.com/Soudeepta146 </p>
         </section>
     )
 };
